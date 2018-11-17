1827ab0be3d63b5e6887b16cfc1bb571f683dea4
Mack Gray <mackenziegray4@gmail.com>
2018-11-17 16:18:47 -0500

    remove all gatsby starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Mack Gray',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,13 +16,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'mackgray.ca',
+        short_name: 'mackgray',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
