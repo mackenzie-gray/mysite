1e518e102a46456a28536f1dab0425c6f7bd83df
Mack Gray <mackenziegray4@gmail.com>
2018-11-18 11:26:16 -0500

    stub out a bunch of content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,32 @@ import styles from './styles/index.module.css'
 const IndexPage = () => (
   <Layout>
     <div className={styles.container}>
-      <h1>Mack Gray</h1>
+      <section id="about">
+        <h1 className="intro">Mack Gray</h1>
+        <h4>Web and app developer from Toronto, ON.</h4>
+      </section>
+      <section id="experience">
+        <h2>Experience</h2>
+        <h4>Wattpad</h4>
+        <h4>AGF, Canada Company</h4>
+        <h4>NexJ Systems</h4>
+      </section>
+      <section id="projects">
+        <h2>Projects</h2>
+        <p>
+          Check out projects I've worked on on my{' '}
+          <a
+            href="https://github.com/mackenzie-gray"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
+        </p>
+      </section>
+      <section id="contact">
+        <h2>Contact</h2>
+      </section>
     </div>
   </Layout>
 )
